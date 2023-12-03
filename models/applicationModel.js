d5e22525b9cbd3f050122fe6707ce3ce03ef8e7c
Kunal Sapate <113632918+kunal20000@users.noreply.github.com>
2023-12-03 18:13:41 +0530

    Update applicationModel.js

--- models/applicationModel.js
+++ models/applicationModel.js
@@ -2,9 +2,27 @@ const mongoose = require('mongoose');
 
 const applicationSchema = new mongoose.Schema({
   // TODO: Define the 'applicantName' field with type String and required validation
+  applicantName:{
+    type: String,
+    required: true,
+  },
   // TODO: Define the 'applicantEmail' field with type String and required validation
+  applicantEmail:{
+    type: String,
+    required: true,
+  },
   // TODO: Define the 'pet' field with type mongoose.Schema.Types.ObjectId, ref to pet, and required validation
+  pet:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: pet,
+    required: true,
+  },
   // TODO: Define the 'status' field with type String, enum ['pending', 'approved', 'rejected'], and default value
+  status:{
+    type: String,
+    enum: ['pending', 'approved', 'rejected'],
+    default: true,
+  },
 });
 
 const Application = mongoose.model('Application', applicationSchema);
