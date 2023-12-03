4ae2c9f24fa17fc8ce54cf70968ae0c805dcec63
Kunal Sapate <113632918+kunal20000@users.noreply.github.com>
2023-12-03 18:16:29 +0530

    Update petModel.js

--- models/petModel.js
+++ models/petModel.js
@@ -3,10 +3,31 @@ const mongoose = require('mongoose');
 // TODO: Define the pet schema with appropriate data types and validation
 const petSchema = new mongoose.Schema({
   // TODO: Define the 'name' field with type String and required validation
+  name:{
+    type: String,
+    required; true,
+  },
   // TODO: Define the 'species' field with type String and required validation
+  species:{
+    type: String,
+    required; true,
+  },
   // TODO: Define the 'age' field with type String and required validation
+  age:{
+    type: String,
+    required: true,
+  },
   // TODO: Define the 'description' field with type String and required validation
+  description:{
+     type: String,
+    required: true,
+  },
   // TODO: Define the 'adoptionStatus' field with type String, enum['available', 'pending', 'adopted'], and default value
+  adoptionStatus:{
+     type: String,
+     enum: ['available', 'pending', 'adopted'],
+     default: true,
+  },
 });
 
 const Pet = mongoose.model('Pet', petSchema);
