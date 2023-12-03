6e523045fa48e1c82f30e58f08285dd59107d3d9
Kunal Sapate <113632918+kunal20000@users.noreply.github.com>
2023-12-03 18:45:31 +0530

    Update petController.js

--- controllers/petController.js
+++ controllers/petController.js
@@ -2,10 +2,24 @@ const Pet = require('../models/petModel');
 
 exports.createPet = async (req, res) => {
   try {
-    // TODO: Extract pet details from the request body
-    // TODO: Create a new pet instance
-    // TODO: Save the new pet to the database
-    // TODO: Respond with the newly created pet
+     // Extract pet details from the request body
+    const { name, species, age, description, adoptionStatus } = req.body;
+
+    // Create a new pet instance
+    const newPet = new Pet({
+      name,
+      species,
+      age,
+      description,
+      adoptionStatus,
+    });
+
+    // Save the new pet to the database
+    const savedPet = await newPet.save();
+
+    // Respond with the newly created pet
+    res.status(201).json(savedPet);
+    
   } catch (error) {
     // Handle errors, such as validation errors or database issues
     res
@@ -17,7 +31,11 @@ exports.createPet = async (req, res) => {
 exports.getAllPets = async (req, res) => {
   try {
     // TODO: Fetch all pets from the database
-    // TODO: Respond with the list of pets
+  
+     const allPets = await Pet.find();
+
+    // Respond with the list of pets
+    res.status(200).json(allPets);
   } catch (error) {
     res.status(500).json({ message: 'Internal server error' });
   }
@@ -28,6 +46,14 @@ exports.getPetById = async (req, res) => {
 
   try {
     // TODO: Find the pet by ID in the database
+     const pet = await Pet.findById(petId);
+
+    if (!pet) {
+      return res.status(404).json({ message: 'Pet not found' });
+    }
+
+    // Respond with the pet details
+    res.status(200).json(pet);
   } catch (error) {
     res.status(500).json({ message: 'Internal server error' });
   }
